f73a7183a7baf5c58c0bfae8b9c94be26832166f
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-10 15:45:41 +0200

    seed done and routes made

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  new_article = Article.new(title: Faker::TvShows::DrWho.quote, content: Faker::TvShows::MichaelScott.quote)
+  new_article.valid?  # => should be `true`
+  new_article.save    # => should not display rollback warnings
+  Article.last        # => should be the one you just saved
+end
